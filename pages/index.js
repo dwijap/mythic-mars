19570861e0049f2bfb68c2f055fbd5ea86d907fd
Dwija Parikh <dwijaparikh@gmail.com>
2022-01-29 16:18:58 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="MYTHIC MARS" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          SEARCH <code>pages/index.js</code>
         </p>
       </main>
 
