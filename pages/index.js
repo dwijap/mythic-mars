0e606ce215a723455803c65287e54af415fdf3d3
Dwija Parikh <dwijaparikh@gmail.com>
2022-01-29 16:52:38 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,18 @@ export default function Home() {
         <Header title="MYTHIC MARS" />
            <script async src="https://cse.google.com/cse.js?cx=0e9acb91c969b373f"></script>
            <div class="gcse-search"></div>
+    
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+        <script type="text/javascript">
+         algoliasearchNetlify({
+         appId: '5SRLHGYB7I',
+         apiKey: '<YOUR_ALGOLIA_SEARCH_API_KEY>',
+         siteId: 'c75b9e6b-e410-4d43-82f6-3cadd245f2b9',
+         branch: 'main',
+         selector: 'div#search',
+         });
+        </script>
       </main>
 
       <Footer />
