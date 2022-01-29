29e9bc81c72102c65677a99137d3d127fbe1f224
Dwija Parikh <dwijaparikh@gmail.com>
2022-01-29 17:45:11 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import landing from 'styles'
 
 export default function Home() {
   return (
